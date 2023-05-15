869a6d542795ad09015b9306f2688aae5d44aab9
Gage Norton <nortongage0@gmail.com>
2023-05-15 10:34:26 -0500

    Add client form

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,29 @@
 import Header from './components/Header.js';
 import { ApolloProvider, ApolloClient, InMemoryCache} from '@apollo/client';
 import Clients from './components/Clients.jsx'
+import AddClientModal from './components/AddClientModal.jsx';
+
+const cache = new InMemoryCache({
+  typePolicies: {
+    Query: {
+      fields: {
+        clients: {
+          merge(existing, incoming) {
+            return incoming;
+          },
+        },
+        projects: {
+          merge(existing, incoming) {
+            return incoming;
+          },
+        },
+      },
+    },
+  },
+});
 
 const client = new ApolloClient({uri: 'http://localhost:3001/graphql',
-cache: new InMemoryCache()
+cache
 })
 
 function App() {
@@ -12,6 +32,7 @@ function App() {
     <ApolloProvider client={client}>
       <Header />
       <div className="container">
+        <AddClientModal />
         <Clients />
       </div>
       </ApolloProvider>
